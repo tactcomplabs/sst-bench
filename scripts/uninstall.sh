d6f1bd044228fb80a3585846753afa77d392a074
John Leidel <john.leidel@gmail.com>
2024-05-11 13:08:04 -0500

    adding micro-comp benchmark component

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,12 +5,14 @@
 
 #-- unregister the libs
 sst-register -u msgperf
+sst-register -u microcomp
 
 #-- forcible remove it from the local script
 CONFIG=~/.sst/sstsimulator.conf
 if test -f "$CONFIG"; then
   echo "Removing configuration from local config=$CONFIG"
   sed -i.bak '/msgperf/d' $CONFIG
+  sed -i.bak '/microcomp/d' $CONFIG
 fi
 
 # EOF
--- test/micro-comp/micro-comp-test1.py
+++ test/micro-comp/micro-comp-test1.py
@@ -1,13 +1,19 @@
 #
-# sst-bench/sst-bench CMake
-#
 # Copyright (C) 2017-2024 Tactical Computing Laboratories, LLC
 # All Rights Reserved
 # contact@tactcomplabs.com
+#
 # See LICENSE in the top level directory for licensing details
 #
+# micro-comp-test1.py
+#
 
-add_subdirectory(msg-perf)
-add_subdirectory(micro-comp)
+import os
+import sst
+
+c0 = sst.Component("c0", "microcomp.MicroComp")
+c0.addParams({
+  "verbose" : 5
+})
 
 # EOF
