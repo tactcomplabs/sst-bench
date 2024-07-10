de2d58eb3d996546e7eebdb844a1af530afef841
John Leidel <john.leidel@gmail.com>
2024-07-10 07:57:58 -0500

    adding LargeStat test to exercise large numbers of statistics for small components

---
+++ test/large-stat/large-stat-test1.py
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2017-2024 Tactical Computing Laboratories, LLC
+# All Rights Reserved
+# contact@tactcomplabs.com
+#
+# See LICENSE in the top level directory for licensing details
+#
+# large-stat-test1.py
+#
+
+import os
+import sst
+
+c0 = sst.Component("c0", "largestat.LargeStat")
+c0.addParams({
+  "verbose" : 5,
+  "numStats" : 10,
+})
+
+# EOF
