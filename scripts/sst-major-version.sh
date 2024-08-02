91a6d056e9082b4b88fcdc57787c867b6b153876
Kenneth Griesser <kgriesser@tclcomplabs.com>
2024-08-02 14:17:55 -0700

    Updated sst version script to support sst devel branch by returning 99 when sst --version is (-dev)

--- scripts/sst-major-version.sh
+++ scripts/sst-major-version.sh
@@ -2,8 +2,14 @@
 #
 # Derives the SST major version
 # In SST 14.0.0; this script returns "14"
+# For development branches (-dev); return "99"
 #
 
-sst --version | awk '{print $3}' | tr -d '()' | awk '{split($0,a,"."); print a[1]}'
+ver=$(sst --version | awk '{print $3}' | tr -d '()')
+if [ "$ver" == "-dev" ]; then
+    echo 99
+else
+    echo $ver | awk '{split($0,a,"."); print a[1]}'
+fi
 
 # EOF
