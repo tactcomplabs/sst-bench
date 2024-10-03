236e9600756265dd8a57e6bc1554ad11b61c7c71
Kenneth Griesser <kgriesser@tclcomplabs.com>
2024-10-03 13:44:44 -0700

    Updated checkpoint tests to use new directory structure in sst-core 0693c068. Temporarily commented out large_stat-chkpnt which are timing out and filling the disk.

---
+++ scripts/sst-chkpt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script is intended to be run in CMakeLists.txt located in tests directories
+# where checkpoints are generated.
+# 
+# If tests that generate checkpoints are run repeatedly, sst will create a directory
+# for each run.
+# 
+# This script deletes the 'first' directory but will leave other 
+# directories (with numeric suffix) alone. Then the targeted sst
+# command will be run.
+
+# Usage: sst-chkpt {checkpoint prefix} {rest of sst options}
+
+if [ -d $1 ]; then
+    echo "WARNING: removing $1"
+    rm -rf ${1}
+fi
+
+args=""
+for (( i==2; i <= "$#"; i++)); do
+    args="$args ${!i}"
+done
+
+echo sst --checkpoint-prefix=$1 $args
+sst --checkpoint-prefix=$1 $args
+
+#EOF
+
