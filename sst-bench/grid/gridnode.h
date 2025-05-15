69475b205f1b51825adbbfbfb674a81851c5f822
Ken Griesser <kgriesser@tactcomplabs.com>
2025-05-14 21:52:04 -0500

    compile using vector type

--- sst-bench/grid/gridnode.h
+++ sst-bench/grid/gridnode.h
@@ -30,9 +30,9 @@
 // clang-format on
 
 // Static TUT selection
-#define TUT uint32_t
+// #define TUT uint32_t
 // #define TUT StructUint8x4
-// #define TUT VecUint8x4
+#define TUT VecUint8x4
 // #define TUT Tuple8x4
 // #define TUT PairPair
 
