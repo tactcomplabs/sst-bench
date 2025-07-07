b985a0ba34837ad231f3a4e3671d5a904eb2414f
John Leidel <john.leidel@gmail.com>
2025-07-07 09:19:51 -0500

    finishing initial noodle code and adding tests

---
+++ test/noodle/noodle-test1.py
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2017-2024 Tactical Computing Laboratories, LLC
+# All Rights Reserved
+# contact@tactcomplabs.com
+#
+# See LICENSE in the top level directory for licensing details
+#
+# noodle-test1.py
+#
+
+import os
+import sst
+
+
+n0 = sst.Component("n0", "noodle.Noodle")
+n0.addParams({
+  "verbose"      : 9,
+  "clockFreq"    : "1GHz",
+  "numPorts"     : 1,
+  "msgPerClock"  : 1,
+  "bytesPerClock": 1,
+  "portsPerClock": 1,
+  "clocks"       : 100000,
+  "rngSeed"      : 3131
+})
+
+n1 = sst.Component("n1", "noodle.Noodle")
+n1.addParams({
+  "verbose"      : 9,
+  "clockFreq"    : "1GHz",
+  "numPorts"     : 1,
+  "msgPerClock"  : 1,
+  "bytesPerClock": 1,
+  "portsPerClock": 1,
+  "clocks"       : 100000,
+  "rngSeed"      : 3131
+})
+
+link0 = sst.Link("link0")
+link0.connect( (n0, "port0", "1us"), (n1, "port0", "1us") )
+
+# EOF
