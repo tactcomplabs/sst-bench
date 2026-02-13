117de224953667c2533ef2e093cbbe990008179b
Kenneth Griesser <kgriesser@tclcomplabs.com>
2026-02-13 08:21:09 -0700

    Fixed issues with SST_VERSION override and updated cmake files to use it

--- scripts/sst-minor-version.sh
+++ scripts/sst-minor-version.sh
@@ -4,6 +4,13 @@
 # In SST 14.1.0; this script returns "1"
 #
 
-sst --version | awk '{print $3}' | tr -d '()' | awk '{split($0,a,"."); print a[2]}'
+# Allow for test audit without messing with sst binary
+if [ "$#" == 1 ]; then
+    sstver="SST-Core Version ($1)"
+else
+    sstver=$(sst --version)
+fi
+
+echo $sstver | awk '{print $3}' | tr -d '()' | awk '{split($0,a,"."); print a[2]}'
 
 # EOF
