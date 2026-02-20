39ac49a60bd4c0d90793540ab408deb5ffd2cba9
Kenneth Griesser <kgriesser@tclcomplabs.com>
2026-02-20 12:18:16 -0700

    setup for 1 node and 4 node slurm runs

--- test/noodle/sweep-examples/run-slurm.sh
+++ test/noodle/sweep-examples/run-slurm.sh
@@ -6,7 +6,10 @@
 #
 # See LICENSE in the top level directory for licensing details
 
-/bin/rm -rf sst-perf* timing.db *.csv *.sql
+# Using SLURM, run weak scaling test from 4 to 40 ranks on 1 node.
+# Then run with ranks evenly distributed on 4 nodes.
+
+/bin/rm -rf sst-perf* timing.db slurm.csv *.sql
 
 SWEEPER=$(realpath ${SST_BENCH_HOME}/scripts/sst-sweeper.py) || exit 1
 JSONCFG=$(realpath ${SST_BENCH_HOME}/test/noodle/sweep.json) || exit 1
@@ -14,9 +17,10 @@ SDL=$(realpath ${SST_BENCH_HOME}/test/noodle/noodle-2d.py) || exit 1
 
 # Do not run checkpoint and restart simulations
 SEQ="--seq=BASE"
-SLURM="--slurm --nodeclamp=4"
+SLURM="--slurm"
 NOPROMPT="--noprompt"
-$SWEEPER ${JSONCFG} ${SDL} weak_scaling_1to8threads_100bto1kb $SEQ $SLURM $NOPROMPT || exit 2
+$SWEEPER ${JSONCFG} ${SDL} weak_scaling_4node --jobname="NODES_4" $SEQ $SLURM $NOPROMPT || exit 2
+$SWEEPER ${JSONCFG} ${SDL} weak_scaling_1node --jobname="NODES_1" $SEQ $SLURM $NOPROMPT || exit 2
 
 # simple sql script to extract some good info
 cat << EOF > query.sql
@@ -27,19 +31,25 @@ create temp table t1 as
 create temp table t2 as
     select jobid, max_build_time, max_run_time, global_max_rss
     from timing_info;
+
+create temp table t3 as
+    select jobid, job_name, slurm_id, nodes
+    from slurm_info;
     
 create temp table report as
-    select * from t1 left join t2 where t1.jobid=t2.jobid;
+    select * from t1
+    left join t2 where t1.jobid=t2.jobid
+    left join t3 where t3.jobid=t3.jobid;
 
 .headers on
 .mode csv
-.output report.csv
+.output slurm.csv
 
-select * from report;
+select * from slurm;
 EOF
 
-# generate report.csv
+# generate slurm.csv
 sqlite3 timing.db < query.sql
 
 # print a few lines
-head report.csv
+head slurm.csv
