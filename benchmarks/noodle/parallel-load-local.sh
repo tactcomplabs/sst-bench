a04cf970fbab00dc180cdd2661f523ea1eb4176d
Kenneth Griesser <kgriesser@tclcomplabs.com>
2026-03-03 12:18:24 -0700

    quick parallel load check

--- benchmarks/noodle/parallel-load-local.sh
+++ benchmarks/noodle/parallel-load-local.sh
@@ -6,9 +6,7 @@
 #
 # See LICENSE in the top level directory for licensing details
 #
-# speed-check.sh
-# Run simulations with 8 ranks and 8 threads (with and without mpi).
-# Visually check results since older versions have changed timing output strings
+# parallel-load-local.sh
 #
 
 if [[ $(uname -o) =~ [Dd]arwin ]]; then
@@ -17,14 +15,16 @@ else
     MPIOPTS="--bind-to socket"
 fi
 
-SST_OPTS="--output-json=config.json --output-partition --print-timing-info ../noodle-bench.py -- --verbose=0 --rngSeed=3131 --numComps=100 --portsPerComp=100 --msgPerClock=8 --bytesPerClock=4 --clocks=10000"
+CLOCKS=300000
+SST_OPTS="--print-timing-info=4 --output-config=config.py ../noodle-bench.py -- --verbose=0 --rngSeed=3131 --numComps=100 --portsPerComp=100 --msgPerClock=8 --bytesPerClock=4 --clocks=$CLOCKS"
 
 echo "########"
 echo "sst -n 8"
 echo "########"
 
 rm -rf sst-n8; mkdir sst-n8 || exit 1
-( cd sst-n8 && sst --num-threads=8 ${SST_OPTS} | tee log )
+( cd sst-n8 && sst --num-threads=8 --parallel-output --run-mode=init ${SST_OPTS} | tee init.log ) || exit 1
+( cd sst-n8 && sst --num-threads=8 --parallel-load config.py | tee run.log ) || exit 1
 
 doit () {
     NP=$1
@@ -34,7 +34,8 @@ doit () {
     echo "########################"
     DIR=mpi-np${NP}-n${N}
     rm -rf $DIR; mkdir $DIR || exit 1
-    ( cd ${DIR} && mpirun $MPIOPTS -np $NP sst --num-threads=$N ${SST_OPTS} | tee log )
+    ( cd ${DIR} && mpirun $MPIOPTS -np $NP sst --num-threads=$N --parallel-output --run-mode=init  ${SST_OPTS} | tee init.log ) || exit 1
+    ( cd ${DIR} && mpirun $MPIOPTS -np $NP sst --num-threads=$N --parallel-load config.py | tee run.log ) || exit 1
 }
 
 #   ranks threads/rank
