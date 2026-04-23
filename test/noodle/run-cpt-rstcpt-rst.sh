83ab9ec16348fc15be8e2dc78e3130e2f60fd064
Kenneth Griesser <kgriesser@tclcomplabs.com>
2026-04-23 13:47:40 -0700

    noodle test sequence to generate a checkpoint, load it and generate new checkpoints, then load them

---
+++ test/noodle/run-cpt-rstcpt-rst.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+#
+# Copyright (C) 2017-2026 Tactical Computing Laboratories, LLC
+# All Rights Reserved
+# contact@tactcomplabs.com
+#
+# See LICENSE in the top level directory for licensing details
+
+/bin/rm -rf crcr_*
+
+ADDLIBPATH="--add-lib-path=../../components/noodle"
+if [ "$SST_VALGRIND" -eq 1 ]; then
+    SST="valgrind sst"
+else
+    SST="sst"
+fi    
+
+echo "### initial checkpoint"
+$SST --checkpoint-sim-period=3us --checkpoint-prefix=crcr_1_cpt noodle-2d.py || exit 1
+
+echo "### load checkpoint and generate new ones"
+$SST --checkpoint-sim-period=2us --checkpoint-prefix=crcr_2_rst_cpt  crcr_1_cpt/crcr_1_cpt_1_3000000/crcr_1_cpt_1_3000000.sstcpt || exit 2
+
+echo "### final checkpoint load "
+$SST  ${ADDLIBPATH}  crcr_2_rst_cpt/crcr_2_rst_cpt_1_4000000/crcr_2_rst_cpt_1_4000000.sstcpt  || exit 31
+$SST  ${ADDLIBPATH}  crcr_2_rst_cpt/crcr_2_rst_cpt_2_6000000/crcr_2_rst_cpt_2_6000000.sstcpt  || exit 32
+$SST  ${ADDLIBPATH}  crcr_2_rst_cpt/crcr_2_rst_cpt_3_8000000/crcr_2_rst_cpt_3_8000000.sstcpt  || exit 33
+$SST  ${ADDLIBPATH} crcr_2_rst_cpt/crcr_2_rst_cpt_4_10000000/crcr_2_rst_cpt_4_10000000.sstcpt || exit 34
+
+/bin/rm -rf crcr_*
+echo "run-cpt-rstcpt-rst.sh passed"
+wait
+
